a56fefbcb4b62b94786743ada67c7cf0ee7477e5
Aan Suseno <aansuseno003@gmail.com>
2019-09-23 07:51:29 +0700

    initial project

---
+++ application/views/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tambah Data</title>
+<link rel="stylesheet" type="text/css" href="<?php echo base_url().'bootstrap/css/bootstrap.css' ?>">
+  <script type="text/javascript"src="<?php 
+  echo base_url().'bootstrap/js/jquery.js'; ?>"></script>
+  <script type="text/javascript" src="<?php 
+  echo base_url().'assets/js/bootstrap.js'; ?>"></script>
+<link rel="stylesheet" type="text/css" href="<?php echo 
+base_url() ?>css.css">
+</head>
+<body>
+	 <div class="container">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">KELOLA DATA BARANG</a>
+  </nav>
---
+++ barang.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 12, 2019 at 11:02 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `barang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `id` int(11) NOT NULL,
+  `kode` varchar(8) COLLATE latin1_general_ci NOT NULL,
+  `nama` varchar(50) COLLATE latin1_general_ci NOT NULL,
+  `deskripsi` text COLLATE latin1_general_ci NOT NULL,
+  `stok` decimal(5,2) NOT NULL,
+  `harga` decimal(12,2) NOT NULL,
+  `berat` decimal(5,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
